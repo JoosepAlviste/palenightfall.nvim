4317de5907ec558f2d832fe6c52ea31640af314e
Joosep Alviste <joosep.alviste@gmail.com>
2022-08-19 23:07:01 +0300

    refactor: use Lua API to create highlights
    Will deprecate the `style` field on the highlight configuration option
    next.

--- lua/palenightfall/internal.lua
+++ lua/palenightfall/internal.lua
@@ -3,18 +3,49 @@ local M = {}
 ---@class PalenightfallHighlight
 ---@field fg string Foreground color
 ---@field bg string Background color
----@field style string
 ---@field sp string Decoration color
+---@field bold boolean
+---@field standout boolean
+---@field underline boolean
+---@field underdouble boolean
+---@field underdotted boolean
+---@field underdashed boolean
+---@field strikethrough boolean
+---@field italic boolean
+---@field reverse boolean
+---@field nocombine boolean
+---@field style string
 
 ---@param group string
 ---@param colors PalenightfallHighlight
 function M.highlight(group, colors)
-  local style = colors.style and 'gui=' .. colors.style or 'gui=NONE'
-  local fg = colors.fg and 'guifg=' .. colors.fg or 'guifg=NONE'
-  local bg = colors.bg and 'guibg=' .. colors.bg or 'guibg=NONE'
-  local sp = colors.sp and 'guisp=' .. colors.sp or ''
+  local style = {
+    bold = colors.style == 'bold',
+    standout = colors.style == 'standout',
+    underline = colors.style == 'underline',
+    undercurl = colors.style == 'undercurl',
+    underdouble = colors.style == 'underdouble',
+    underdotted = colors.style == 'underdotted',
+    underdashed = colors.style == 'underdashed',
+    strikethrough = colors.style == 'strikethrough',
+    italic = colors.style == 'italic',
+    reverse = colors.style == 'reverse',
+    nocombine = colors.style == 'nocombine',
+  }
 
-  vim.cmd(string.format('highlight %s %s %s %s %s', group, style, fg, bg, sp))
+  if colors.style ~= nil then
+    colors.style = nil
+  end
+
+  vim.api.nvim_set_hl(
+    0,
+    group,
+    vim.tbl_extend('force', style, colors, {
+      fg = colors.fg or 'NONE',
+      bg = colors.bg or 'NONE',
+      sp = colors.sp or 'NONE',
+    })
+  )
 end
 
 return M
