aaf54a2ddd6908b8b9a8647217535f73cac2d4ea
Joosep Alviste <joosep.alviste@gmail.com>
2022-09-30 13:31:53 +0300

    feat(cmp): improve cmp menu highlights
    I had some problems getting the cmp highlights to apply, you can check
    out this commit in my dotfiles if you're running into issues yourself:
    https://github.com/JoosepAlviste/dotfiles/commit/ca05189b3046108f592fd1606a37cc2006fa3374
    
    I haven't figured out a good solution for that specific problem yet
    though...

--- lua/palenightfall/init.lua
+++ lua/palenightfall/init.lua
@@ -87,7 +87,7 @@ function M.configure_highlights(overrides)
     PMenu        = { bg = c.background_darker },
     PMenuSBar    = { bg = c.background_darker },
     PMenuThumb   = { bg = c.background },
-    PMenuSel     = { fg = c.cyan, bg = c.background },
+    PMenuSel     = { bg = c.highlight },
     NormalFloat  = { bg = c.background },
     FloatBorder  = { fg = c.foreground_darker },
     Question     = { fg = c.green },
@@ -191,9 +191,46 @@ function M.configure_highlights(overrides)
     diffRemoved = { fg = c.red },
 
     -- hrsh7th/nvim-cmp
-    CmpDocumentation   = { bg = c.background_darker },
+    CmpDocumentation = { bg = c.background_darker },
     CmpItemAbbrDefault = { fg = c.foreground },
+
+    CmpItemAbbr = { fg = c.foreground },
+    CmpItemAbbrDeprecated = { fg = c.foreground_darker, strikethrough = true },
+    CmpItemAbbrMatch = { fg = c.blue },
+    CmpItemAbbrMatchFuzzy = { fg = c.blue },
+
+    CmpItemMenu = { fg = c.comments },
+
     CmpItemKindDefault = { fg = c.foreground_darker },
+
+    CmpItemKindKeyword = { fg = c.purple },
+    CmpItemKindReference = { fg = c.purple },
+    CmpItemKindValue = { fg = c.purple },
+    CmpItemKindVariable = { fg = c.purple },
+
+    CmpItemKindConstant = { fg = c.orange },
+    CmpItemKindEnum = { fg = c.orange },
+    CmpItemKindEnumMember = { fg = c.orange },
+    CmpItemKindOperator = { fg = c.orange },
+    CmpItemKindField = { fg = c.orange },
+
+    CmpItemKindFunction = { fg = c.blue },
+    CmpItemKindMethod = { fg = c.blue },
+
+    CmpItemKindConstructor = { fg = c.yellow },
+    CmpItemKindClass = { fg = c.yellow },
+    CmpItemKindInterface = { fg = c.yellow },
+    CmpItemKindStruct = { fg = c.yellow },
+    CmpItemKindEvent = { fg = c.yellow },
+    CmpItemKindUnit = { fg = c.yellow },
+    CmpItemKindFile = { fg = c.yellow },
+    CmpItemKindFolder = { fg = c.yellow },
+
+    CmpItemKindModule = { fg = c.green },
+    CmpItemKindProperty = { fg = c.green },
+    CmpItemKindTypeParameter = { fg = c.green },
+    CmpItemKindSnippet = { fg = c.green },
+    CmpItemKindText = { fg = c.green },
 
     -- nvim-telescope/telescope.nvim
     TelescopeMatching       = { fg = c.blue },
